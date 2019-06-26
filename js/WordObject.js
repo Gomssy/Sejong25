6f871788c4ac60535233f563f0b45042f9949df1
jasson31 <jasson31@naver.com>
2019-06-27 02:12:45 +0900

    코드 간략화

--- js/WordObject.js
+++ js/WordObject.js
@@ -2,7 +2,10 @@ class WordObject
 {
     constructor(text)
     {
+        this.generationCode = WordSpace.nextWordCode++;
+
         this.wordText = text;
+        //this.wordText = Input.removeConVow(text);
         this.wordTyping = (function(_wordText)
         {
             var temp = 0;
@@ -23,17 +26,27 @@ class WordObject
                 _wordGrade == 1 ? 7 : 10;
             return temp;
         })(this.wordGrade);
-        //alert("wordTyping : " + this.wordTyping + '\n' + "wordGrade : " + this.wordGrade + '\n' + "wordWeight : " + this.wordWeight + '\n');
+        //console.log("wordTyping : " + this.wordTyping + '\n' + "wordGrade : " + this.wordGrade + '\n' + "wordWeight : " + this.wordWeight + '\n');
     }
 
-    generate(scene)
+    instantiate(scene)
     {
-        var randomX = Phaser.Math.Between(100, 500);
-        this.physicsObj = scene.physics.add.sprite(randomX, 100, 'wordBackground').setScale(0.5);
+        var randomX = Phaser.Math.Between(100, 700);
+        this.physicsObj = scene.physics.add.sprite(randomX, 100, 'wordBackground').setScale(0.3);
         this.wordObj = scene.add.text(randomX, 100, this.wordText, {fontFamily: '"궁서", 궁서체, serif'}).setColor('#000000');
         this.wordObj.setOrigin(0.5,0.5);
     }
 
+    destroy()
+    {
+        this.wordObj.destroy();
+        const groupIdx = WordSpace.wordGroup.findIndex(function(item) {return this.isEqualObject(item.generationCode)}, this);
+        if (groupIdx > -1) WordSpace.wordGroup.splice(groupIdx, 1);
+        const forceIdx = WordSpace.wordForcedGroup.findIndex(function(item) {return this.isEqualObject(item.generationCode)}, this);
+        if (forceIdx > -1) WordSpace.wordForcedGroup.splice(forceIdx, 1);
+        WordSpace.wordPhysicsGroup.remove(this.physicsObj, true, true);
+    }
+    
     attract(wordSpeed)
     {
         var dist = Phaser.Math.Distance.Between(this.physicsObj.x, this.physicsObj.y, WordSpace.gravityPoint.x, WordSpace.gravityPoint.y);
@@ -42,30 +55,9 @@ class WordObject
         this.wordObj.setPosition(this.physicsObj.x, this.physicsObj.y);
     }
 
-    getWordWeight()
-    {
-        return this.wordWeight;
-    }
+    getWordWeight() { return this.wordWeight; }
 
-    removeConVow(_wordText)
-    {
-        var temp = 0;
-        for(var i = 0; i < _wordText.length; i++)
-        {
-            
-        }
-        return temp;
-    }
-
-    //***********ToDo*************
-    isEqual(inputWord)
-    {
-        if(inputWord === this.wordText)
-        {
-            this.destroy();
-        }
-    }
-    //****************************
+    isEqualObject(_generationCode) { return _generationCode === this._generationCode; }
 }
 
 function firstSound(charText)
