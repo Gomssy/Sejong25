36074e7921ed364a1d98eec2c30dd869238210bd
김상언 <gggomksy@naver.com>
2019-06-28 21:05:30 +0900

    grade 인자로 받아서 단어 선택하도록 selectWord 수정

--- js/select.js
+++ js/select.js
@@ -16,22 +16,15 @@ Select.selectstartWord = function() {
     Select.startWordList.push(Phaser.Utils.Array.GetRandom(CSVParsing.gradeArray.grade1));
 }
 
-Select.selectWord = function() {
-    var GradeProb = []; //default: ?
-    var rand = Math.random();
-    var selection;
-
-    if(rand<GradeProb[0]) {
-        //3급 단어 선택
-        selection = Phaser.Utils.Array.GetRandom(CSVParsing.gradeArray.grade3);
-    } else if(rand>=GradeProb[0] && rand<GradeProb[1]) {
-        //2급 단어 선택
-        selection = Phaser.Utils.Array.GetRandom(CSVParsing.gradeArray.grade2);
-    } else if(rand>=GradeProb[1] && rand<GradeProb[2]) {
-        //1급 단어 선택
-        selection = Phaser.Utils.Array.GetRandom(CSVParsing.gradeArray.grade1);
-    } else {
-        //특급 단어 선택
-        selection = Phaser.Utils.Array.GetRandom(CSVParsing.gradeArray.grade0);
+Select.selectWord = function(grade) {
+    var selection = "";
+    switch(grade)
+    {
+        case 3: selection = Phaser.Utils.Array.GetRandom(CSVParsing.gradeArray.grade3); break;
+        case 2: selection = Phaser.Utils.Array.GetRandom(CSVParsing.gradeArray.grade2); break;
+        case 1: selection = Phaser.Utils.Array.GetRandom(CSVParsing.gradeArray.grade1); break;
+        case 0: selection = Phaser.Utils.Array.GetRandom(CSVParsing.gradeArray.grade0); break;
+        default: break;
     }
+    return selection;
 }
