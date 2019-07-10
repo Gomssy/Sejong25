a355eadd7111ffe9e3b810f00bde7e757727ad80
Jae Min Kim <jmk1806@cnsh.hs.kr>
2019-07-10 21:57:27 +0900

    음악 처음부터 시작, 반복 성공 / 넘어가면 멈추게 해야함

--- js/BGMsound.js
+++ js/BGMsound.js
@@ -2,27 +2,29 @@ var Audio = Audio || {}
 
 Audio.loadSound = function(scene)
 {
-    scene.load.audio('menuBackground', 'assets/sound/BGM_twochae.ogg')
+    scene.load.audio('BGM', 'assets/sound/BGM_twochae.ogg');
 }
 
 Audio.playSound = function(scene)
 {
-    bgm = scene.sound.play('menuBackground')
+    var bgm = scene.sound.add('BGM');
+    bgm.setLoop(true);
+    bgm.play();
+
 }
 
 Audio.pauseSound = function(scene)
 {
-    bgm = scene.sound.pause()
+    bgm = scene.sound.pause();
 }
 
 Audio.resumeSound = function(scene)
 {
-    bgm = scene.sound.resume()
+    bgm = scene.sound.resume();
 }
 
 Audio.stopSound = function(scene)
 {
-    bgm = scene.sound.stop()
+    bgm = scene.sound.stop();
 }
-// var Audio = new Audio('assets/sound/BGM_twochae.ogg');
-// Audio.play();
+
