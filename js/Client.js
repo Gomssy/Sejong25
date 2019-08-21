9a7701b14ff1d8c417b21677d9cda24aabc9aa26
Jae Min Kim <jmk1806@cnsh.hs.kr>
2019-08-21 20:15:59 +0900

    승리/패배 시 소리 나오게 함

--- js/Client.js
+++ js/Client.js
@@ -124,6 +124,7 @@ socket.on('startGame', function()
 // in game
 socket.on('changePhase', function(msg) // number Phase
 {
+    // Audio.killSound('gameScene',) -> 여기다 게임 페이즈 bgm 넣으면 됨
     WordSpace.changePhase(msg);
 });
 socket.on('setPlayerTypingRate', function(msg) // number playerTypingRate
@@ -203,6 +204,13 @@ socket.on('defeat', function(msg) // object player
     let playerImage = RoomData.findPlayer(msg.id).playerImage;
     let position = RoomData.findPlayer(msg.id).position;
     let nicknameText = RoomData.findPlayer(msg.id).nicknameText;
+
+    // 이거 조건문으로 페이즈마다 다르게 해줘야 하는데 모르겠음
+    Audio.killSound(ScenesData.gameScene, 'Phase1');
+    Audio.killSound(ScenesData.gameScene, 'Phase2');
+    Audio.killSound(ScenesData.gameScene, 'Phase3');
+
+    Audio.playSound(ScenesData.gameScene, 'defeat');
     RoomData.players[msg.index] = msg;
     RoomData.players[msg.index].playerImage = playerImage;
     RoomData.players[msg.index].position = position;
@@ -355,6 +363,13 @@ socket.on('defeat', function(msg) // object player
 socket.on('gameEnd', function(msg) // number winnerId
 {
     const winner = RoomData.findPlayer(msg);
+
+    // 패배랑 같은 이유의 조건문 필요
+    Audio.killSound(ScenesData.gameScene, 'Phase1');
+    Audio.killSound(ScenesData.gameScene, 'Phase2');
+    Audio.killSound(ScenesData.gameScene, 'Phase3');
+
+    Audio.playSound(ScenesData.gameScene, 'victory');
     console.log(winner.nickname + ' Win!!!!!!');
     if(msg == RoomData.myself.id)
     {
